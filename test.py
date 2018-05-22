7f9079d5d33a9b5811b8f5db1b6a405f99dcc8ce
leonyue <leonyue@tencent.com>
2018-05-22 21:23:40 +0800

    save test result

--- test.py
+++ test.py
@@ -1,6 +1,42 @@
 from tf_model import CnnMaxPool
 import tensorflow as tf
 model = CnnMaxPool()
-tf.train.Saver().restore(model.sess, "data/save/model_2_256")
+tf.train.Saver().restore(model.sess, "data/save/model")
 accuracy, recall, f1 = model.test(model.loader.test)
 print(accuracy, recall, f1)
+
+result = []
+inputs = []
+lens = []
+labels = []
+for input, label in model.loader.test:
+    inputs.append(input)
+    lens.append(len(input))
+    labels.append(label)
+    if len(inputs) >= 128:
+        max_len = max(lens)
+        model.loader.padding(inputs, max_len)
+        feed_dict = {
+            model.inputs: inputs,
+            model.lens: lens,
+            model.labels: labels,
+        }
+        predictions = model.sess.run(model.logits, feed_dict=feed_dict)
+        result.extend(predictions)
+        inputs = []
+        lens = []
+        labels = []
+if len(inputs) > 0:
+    max_len = max(lens)
+    model.loader.padding(inputs, max_len)
+    feed_dict = {
+        model.inputs: inputs,
+        model.lens: lens,
+        model.labels: labels,
+    }
+    predictions = model.sess.run(model.logits, feed_dict=feed_dict)
+    result.extend(predictions)
+
+with open('predictions_2.txt') as f:
+    for r in result:
+        f.write(str(r) + '\n')
